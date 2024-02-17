a8f159cac0e56223387fbe91fbf0d8eae5fa8d68
Gassama <gassma803@gmail.com>
2024-02-17 03:56:50 +0100

    get a product ok

--- src/main/java/com/gaslandie/springbootWepApp1/ProductController.java
+++ src/main/java/com/gaslandie/springbootWepApp1/ProductController.java
@@ -2,11 +2,14 @@ package com.gaslandie.springbootWepApp1;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.websocket.server.PathParam;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +28,7 @@ public class ProductController {
     }
 
     @GetMapping("/product/{id}")
-    public Product getProduct(@RequestParam int id) {
+    public Product getProduct(@PathVariable int id) {
         return service.getProduct(id);
     }
     @PostMapping("/product")
