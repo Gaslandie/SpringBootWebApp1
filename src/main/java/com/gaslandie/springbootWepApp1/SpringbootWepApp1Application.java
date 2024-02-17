1567873599867e2c5ffd0016e9f26dd104572412
Gassama <gassma803@gmail.com>
2024-02-17 01:52:30 +0100

    initialisation du projet Spring boot

---
+++ src/main/java/com/gaslandie/springbootWepApp1/SpringbootWepApp1Application.java
@@ -0,0 +1,13 @@
+package com.gaslandie.springbootWepApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWepApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWepApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaslandie/springbootWepApp1/SpringbootWepApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaslandie.springbootWepApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWepApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
